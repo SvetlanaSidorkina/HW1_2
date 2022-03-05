54f57a6ec98b7b4498dafcf61a19e3f4c8880d15
SvetlanaSidorkina <sosvet90@mail.ru>
2022-03-05 18:40:45 +0300

    Create new project

---
+++ app/src/main/java/com/example/homework1ex2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.homework1ex2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
